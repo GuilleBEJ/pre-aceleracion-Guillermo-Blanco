03212ea792bbcdbb588ee90f54be5adcd93069eb
GuilleBEJ <guillermoblancovargas@hotmail.com>
2022-07-10 23:19:55 -0300

    Initial commit

--- src/main/java/com/alkemy/pre/aceleracion/AceleracionApplication.java
+++ src/main/java/com/alkemy/pre/aceleracion/AceleracionApplication.java
@@ -1,4 +1,4 @@
-package com.example.aceleracion.aceleracion;
+package com.alkemy.pre.aceleracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/alkemy/pre/aceleracion/AceleracionApplicationTests.java
+++ src/test/java/com/alkemy/pre/aceleracion/AceleracionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.aceleracion.aceleracion;
+package com.alkemy.pre.aceleracion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
